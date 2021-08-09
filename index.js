1c8d7e06b862930214daf7b4c2c3c3295a976250
Nafis Rahman <nmr278@nyu.edu>
2021-08-09 10:24:59 -0400

    added generateMarkdown functions

--- index.js
+++ index.js
@@ -39,28 +39,150 @@ DEPENDENCIES
 
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 /*=====================
 DATA
 =====================*/
 
+const fileName = 'README2.md';
 // Array of questions for user input
-const questions = [];
+const questions = [
+    {
+        question: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        question: 'Describe your project.',
+        name: 'description'
+    },
+    {
+        question: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        name: 'installation'
+    },
+    {
+        question: 'Provide instructions and examples for use.',
+        name: 'usage'
+    },
+    {
+        question: 'Please select the license you would like to choose:',
+        name: 'license',
+        choices: ['MIT', 'GNU GPLv3', 'Apache License 2.0']
+    },
+    {
+        question: 'List your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.\nIf you followed tutorials, include links to those here as well.',
+        name: 'credits'
+    },
+    {
+        question: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.',
+        name: 'contributing'
+    },
+    {
+        question: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.',
+        name: 'tests'
+    },
+    {
+        question: 'What is your GitHub username?',
+        name: 'gitHubUsername'
+    },
+    {
+        question: 'What is your email address?',
+        name: 'emailAddress'
+    },
+    {
+        question: 'Table of Contents:',
+        name: 'tableOfContents',
+        choices: [
+            'Description', 
+            'Table of Contents', 
+            'Installation', 
+            'Usage', 
+            'License', 
+            'Contributing', 
+            'Tests', 
+            'Questions'
+        ]
+    }
+];
 
 /*=====================
 FUNCTIONS
 =====================*/
 
 // Function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Commit logged!')
+    );
+};
 
 // Function to initialize app
-function init() {}
-
-/*=====================
-USER INTERACTIONS
-=====================*/
-
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: questions[0].question,
+                name: questions[0].name
+            },
+            {
+                type: 'input',
+                message: questions[1].question,
+                name: questions[1].name
+            },
+            {
+                type: 'input',
+                message: questions[2].question,
+                name: questions[2].name
+            },
+            {
+                type: 'input',
+                message: questions[3].question,
+                name: questions[3].name
+            },
+            {
+                type: 'list',
+                message: questions[4].question,
+                name: questions[4].name,
+                choices: questions[4].choices
+            },
+            {
+                type: 'input',
+                message: questions[5].question,
+                name: questions[5].name
+            },
+            {
+                type: 'input',
+                message: questions[6].question,
+                name: questions[6].name
+            },
+            {
+                type: 'input',
+                message: questions[7].question,
+                name: questions[7].name
+            },
+            {
+                type: 'input',
+                message: questions[8].question,
+                name: questions[8].name
+            },
+            {
+                type: 'input',
+                message: questions[9].question,
+                name: questions[9].name
+            },
+            {
+                type: 'list',
+                message: questions[10].question,
+                name: questions[10].name,
+                choices: questions[10].choices
+            }
+        ])
+        .then((answers) => {
+            console.log(answers)
+            writeToFile(fileName, generateMarkdown(answers))
+        })
+}
 
 /*=====================
 INITIALIZATION
