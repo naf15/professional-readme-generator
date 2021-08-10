c79445b8e3826e91291e4f956ac88bc54c2e6de1
Nafis Rahman <nmr278@nyu.edu>
2021-08-09 21:49:28 -0400

    formatted generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,9 @@
+const licenseLinks = {
+  'MIT' : '[![license](https://img.shields.io/badge/license-${data.license}-blue)](https://shields.io)',
+  'GNU GPLv3' : '', 
+  'Apache License 2.0' : ''  
+}
+
 function renderTitle(title) {!title ? '' : title};
 
 function renderDescriptionSection(description) {!description ? '' : description};
@@ -10,11 +16,11 @@ function renderCreditsSection(credits) {!credits ? '' : credits};
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {!license ? '' : license.badge};
+function renderLicenseBadge(license) {!license ? '' : `[![license](https://img.shields.io/badge/license-${license}-blue)](https://shields.io)`};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {!license ? '' : license.link};
+function renderLicenseLink(license) {!license ? '' : `https://choosealicense.com/licenses/${license}`};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -24,11 +30,23 @@ function renderContributingSection(contributing) {!contributing ? '' : contribut
 
 function renderTestsSection(tests) {!tests ? '' : tests};
 
-function renderQuestionsSection(questions) {!questions ? '' : questions}; 
+function renderQuestionsSection(emailAddress, gitHubUsername) {
+  cont github = `[GitHub](http://github.com${gitHubUsername})`;
+  
+  if (!emailAddress && !gitHubUsername) {
+    return ''
+  } else if (!emailAddress) {
+    return `Feel free to email me at ${emailAddress} if you have any further questions.`
+  } else if (!gitHubUsername) {
+    return  `You can find my github at ${github}.` 
+  } else {
+    return `You can find my github at ${github}. Feel free to email me at ${emailAddress} if you have any further questions.`
+  };
+}; 
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${renderTitle(data.title)} ${renderLicenseBadge(data.license)}
+  return `# ${renderTitle(data.title)} ${renderLicenseBadge(licenseBadges[data.license])}
 
 ## Description
 
